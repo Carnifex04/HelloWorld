26a386a66b2e7932a319ebcc044dc642124d5a7d
hriday <guptahriday4444@gmail.com>
2022-12-15 15:30:11 +0530

    changes for hosting-5

--- videochat/settings.py
+++ videochat/settings.py
@@ -26,9 +26,11 @@ SECRET_KEY = 'django-insecure-)72xl4-2rs17i4t8_^w355_2p2c!fb!7kfv-n%db2jl=bt$#=k
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['web-production-d89d.up.railway.app']
+ALLOWED_HOSTS = ['web-production-d89d.up.railway.app',
+                 'https://web-production-d89d.up.railway.app/', 'http://web-production-d89d.up.railway.app/']
 
-CSRF_TRUSTED_ORIGINS = ['web-production-d89d.up.railway.app']
+CSRF_TRUSTED_ORIGINS = ['web-production-d89d.up.railway.app',
+                        'https://web-production-d89d.up.railway.app/', 'http://web-production-d89d.up.railway.app/']
 
 
 # Application definition
